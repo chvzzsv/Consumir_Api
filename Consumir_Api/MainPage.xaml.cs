423b5d7fa1998320f3d22cf53a2a20b0048d575d
chvzzsv <roscamchav70@gmail.com>
2024-04-23 12:08:34 -0600

    Agregar archivos de proyecto.

---
+++ Consumir_Api/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace Consumir_Api
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public class API
+        {
+            public int userId { get; set; }
+            public int id { get; set; }
+            public string title { get; set; }
+            public string body { get; set; }
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var request = new HttpRequestMessage();
+                    request.RequestUri = new Uri("https://jsonplaceholder.typicode.com/todos");
+                    request.Method = HttpMethod.Get;
+                    request.Headers.Add("Accept", "application/json");
+
+                    HttpResponseMessage response = await client.SendAsync(request);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string content = await response.Content.ReadAsStringAsync();
+                        var resultado = JsonConvert.DeserializeObject<List<API>>(content);
+                        ListDemo.ItemsSource = resultado;
+                    }
+                    else
+                    {
+                        await DisplayAlert("Error", $"Failed to get data: {response.ReasonPhrase}", "OK");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", $"An error occurred: {ex.Message}", "OK");
+            }
+        }
+    }
+}
